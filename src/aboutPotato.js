c0cfac9552a41ef6962ef981c17acea709d5f7df
PotatoHalez <potatohalez@gmail.com>
2023-12-26 23:16:20 -0600

    potato paradise begins

---
+++ src/aboutPotato.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './css/AboutPotato.css'
+
+const AboutPotato = () => {
+    return (
+        <div className='aboutPotato'>
+            <img src='images/potatoparadise.jpeg' alt='Potato Paradise' />
+            <h1> What is Potato Paradise? </h1><p>Welcome to Potato Paradise, the spud-tacular utopia where the humble potato reigns supreme!
+                 This is a land where carbs are not the enemy, and every day is a fry-day. 
+                 Inspired by the snack-sneaking shenanigans of Sasha Blouse from Attack on Titan, 
+                 we've peeled back the layers to bring you a mash-up of games, 'tater-themed inspiration, and a feast of potato pics that'll have you saying, "I yam what I yam!"
+                 So, grab your fork and get ready to dig into the soil-rich wonders of Potato Paradise—where it's okay to be a little bit starchy and a whole lot of fun!</p>
+            
+            <h1> Who is Potato Girl? </h1><p> Potato girl is.. -drum roll- Sasha Blouse! Famously dubbed "Potato Girl," she is the culinary maverick of the Attack on Titan world.
+                With an appetite as fierce as her archery skills, Sasha's love for food is almost as strong as her loyalty to her comrades.
+                In a land shadowed by giant threats, she still manages to sniff out a baked potato, turning any dire scenario into an impromptu snack time.
+                Unapologetically munching in the face of danger, Sasha's the hero every lunch break deserves.
+                Whether it's a battle against titans or a race to the dinner plate, she's your potato girl. Just don't come between her and her carbs! 
+                </p>
+            </div>
+    );
+};
+
+export default AboutPotato;
