4b0ddff3485c7156cde6bfcb17a461401a689713
PotatoHalez <potatohalez@gmail.com>
2023-12-29 23:52:22 -0600

    Sample landing pages

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Home = () => {
+    return <h1> Official home page </h1>
+}
+
+export default Home
---
+++ src/pages/PotatoPics.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PotatoPics = () => {
+    return <h1> Pretend like theres lots of delicious potatoes right here </h1>
+    
+}
+
+export default PotatoPics
