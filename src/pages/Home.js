adac82ebcede5c20f3cb9808a38c970ac8fd7075
PotatoHalez <potatohalez@gmail.com>
2024-01-04 17:16:36 -0600

    Functionality for login for h ome

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,27 @@ import React from 'react'
 import '../css/PotatoHouse.css'
 
 const Home = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('Just kidding! No login needed (yet) to enjoy Potato Paradise!');
+    }
     
     return ( 
-        <>
+        <div className='potatoHouse'>
     <h1> Welcome home, potato lover! </h1>
-    <div className='potatoHouse'>
-        <img src='images/potatohouse.jpeg' alt='Potato House' />
+    <div className='loginContainer'>
+            <img src='images/potatohouse.jpeg' alt='Potato Paradise' />
+            <div className='loginSection'>
+                <h2> Log into Potato Paradise here!</h2>
+            <form onSubmit={handleSubmit} className='loginForm'>
+                <input type='text' placeholder= 'Potatoname' />
+                <input type='text' placeholder='Potatoword' />
+                <button type='submit'>Potato time</button>
+                </form>
+            </div>
+            </div>
     </div>
-    </>
     )
 }
 
