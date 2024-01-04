bc2c094da7559862f473ba48dc4aa5fed55f1767
PotatoHalez <potatohalez@gmail.com>
2024-01-04 17:16:44 -0600

    Added another quote

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Quotes = () => {
-    return <h1> "Are you asking me why people eat potatoes, sir?" </h1>
+    return <><h1> "Are you asking me why people eat potatoes, sir?" </h1>
+    <h1> "You seriously don't know why humans beings eat potatoes?" </h1>
+    </>
     
 }
 
