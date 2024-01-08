0801588a6cca2b131fd0c34296c75df6f4d4c501
PotatoHalez <potatohalez@gmail.com>
2024-01-07 23:55:12 -0600

    Fixed failed test, naming issue

--- src/utils/test/PotatoPics.test.js
+++ src/utils/test/PotatoPics.test.js
@@ -3,6 +3,6 @@ import PotatoPics from '../../pages/PotatoPics'
 
 test('renders PotatoPics correctly', () => {
     render(<PotatoPics />)
-    const PotatoPics = screen.getByText('The land of delicious potatoes!')
-    expect(PotatoPics).toBeInTheDocument();
+    const potatoPics = screen.getByText('The land of delicious potatoes!')
+    expect(potatoPics).toBeInTheDocument();
 });
