2196414b6a1f7864f8f66d55548d00cf67582599
PotatoHalez <potatohalez@gmail.com>
2024-01-08 23:58:54 -0600

    Finishing quotes test

--- src/utils/test/Quotes.test.js
+++ src/utils/test/Quotes.test.js
@@ -3,6 +3,8 @@ import Quotes from '../../pages/Quotes'
 
 test('renders PotatoPics correctly', () => {
     render(<Quotes />)
-    const quotes = screen.getByText('"Are you asking me why people eat potatoes, sir?"')
-    expect(quotes).toBeInTheDocument();
+    const firstQuote = screen.getByText('"Are you asking me why people eat potatoes, sir?"');
+    const secondQuote = screen.getByText('"You seriously don\t\ know why humans beings eat potatoes?"');
+    expect(firstQuote).toBeInTheDocument();
+    expect(secondQuote).toBeInTheDocument();
 });
