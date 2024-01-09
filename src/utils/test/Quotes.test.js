5f93a5125b0a597f0d3150424394b9b93a144aeb
PotatoHalez <potatohalez@gmail.com>
2024-01-09 00:01:31 -0600

    Finished test by escaping the 't

--- src/utils/test/Quotes.test.js
+++ src/utils/test/Quotes.test.js
@@ -4,7 +4,7 @@ import Quotes from '../../pages/Quotes'
 test('renders PotatoPics correctly', () => {
     render(<Quotes />)
     const firstQuote = screen.getByText('"Are you asking me why people eat potatoes, sir?"');
-    const secondQuote = screen.getByText('"You seriously don\t\ know why humans beings eat potatoes?"');
+    const secondQuote = screen.getByText('"You seriously don\'t know why humans beings eat potatoes?"');
     expect(firstQuote).toBeInTheDocument();
     expect(secondQuote).toBeInTheDocument();
 });
