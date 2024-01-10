edde7c10b9c6546a9aa03d433b4325d709f27808
PotatoHalez <potatohalez@gmail.com>
2024-01-09 21:53:33 -0600

    Theme to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 
-const Navbar = () => {
+const Navbar = ( { toggleTheme }) => {
     return (
         <nav className='navbar'>
+            <button onClick={toggleTheme}> Change Theme</button>
             <Link to ='/'>Home</Link>
             <Link to ='/AboutPotato'>About Potato</Link>
             <Link to ='/Quotes'>Quotes</Link>
