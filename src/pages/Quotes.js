c7853db77eccde23da152b2c672de7b9100bdd79
PotatoHalez <potatohalez@gmail.com>
2024-01-24 23:35:08 -0600

    Potato quote

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -4,6 +4,7 @@ const Quotes = () => {
     return <><h1> "Are you asking me why people eat potatoes, sir?" </h1>
     <h1> "You seriously don't know why humans beings eat potatoes?" </h1>
     <h1> "I am potato girl" </h1>
+    <h1> Not everyone can be a truffle. Most of us are potatoes. And a potato is a very good thing to be. Potatoes make everything okay </h1>
 
     </>
     
